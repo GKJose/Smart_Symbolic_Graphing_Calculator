15e7d70edda6116946ab85cb2cae04909d1c685b
Michael Gober <gobermichael13@gmail.com>
2022-02-23 11:17:24 -0600

    Added skeleton for tabview.

--- lv_demos/src/lv_demo_screens/calculator_screens.h
+++ lv_demos/src/lv_demo_screens/calculator_screens.h
@@ -6,7 +6,7 @@ extern "C" {
     void lv_textarea_input(void);
     void lv_textarea_output(void);
     void main_screen_driver(void);
-    
+    void lv_main_screen_tabs(void);
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif
