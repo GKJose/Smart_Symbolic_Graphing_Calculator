d7c785d27196381c73399ec8105c5a9c63400b64
Michael Gober <gobermichael13@gmail.com>
2022-02-26 15:18:30 -0600

    Changed header file.

--- lv_demos/src/lv_demo_screens/main_screen.hxx
+++ lv_demos/src/lv_demo_screens/main_screen.hxx
@@ -4,4 +4,3 @@
     lv_obj_t* lv_textarea_input(lv_obj_t* parent);
     lv_obj_t* lv_textarea_output(lv_obj_t* parent);
     void main_screen_driver(lv_obj_t* parent);
-    void lv_main_screen_tabs(void);
