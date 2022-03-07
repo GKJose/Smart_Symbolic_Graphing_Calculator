8d76e02c7d2fb2db30b453dd421f00021124fb3f
GKJose <mendezj8121@uhcl.edu>
2022-03-07 00:50:47 +0000

    added brightness title

--- Settings.cpp
+++ Settings.cpp
@@ -2,6 +2,7 @@
 
 static lv_obj_t * slider;
 static lv_obj_t * slider_label;
+static lv_obj_t * slider_title;
 static void slider_event_cb(lv_event_t * e)
 {
 
@@ -26,9 +27,14 @@ void createSettingsTab(lv_obj_t* parent){
     /*Create a label below the slider*/
     slider_label = lv_label_create(parent);
     lv_label_set_text(slider_label, "0%");
-
     lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+    
+    slider_title = lv_label_create(parent);
+    lv_label_set_text(slider_title,"Brightness");
+    lv_obj_align_to(slider_title,slider,LV_ALIGN_OUT_TOP_MID,0,0);
+    
     lv_slider_set_range(slider, 25 , 100);
+    
     #if ENABLE_PI
     softPwmCreate(5,100,100);
     #endif
